5fe6f6cb50341fc88951cbd6197728841e1c96e1
Jose M. <joseernestomadlangbayan@gmail.com>
2026-08-04 14:26:47 -0400

    Remove openLightbox function from script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,1 @@
-function openLightbox(src) {
-    const modal = document.getElementById('image-modal');
-    const modalImg = document.getElementById('modal-img');
-    modalImg.src = src;
-    modal.showModal();
-}
+
