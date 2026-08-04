6369445f446432e87496045efadc7285dff6e09f
Jose M. <joseernestomadlangbayan@gmail.com>
2026-08-04 14:12:03 -0400

    Add lightbox functionality to display images

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+<script>
+    function openLightbox(src) {
+        const modal = document.getElementById('image-modal');
+        const modalImg = document.getElementById('modal-img');
+        modalImg.src = src;
+        modal.showModal();
+    }
+</script>
