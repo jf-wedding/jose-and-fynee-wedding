70e359483f16d49e7ed34e76b276f69e054b2e0f
Jose M. <joseernestomadlangbayan@gmail.com>
2026-08-04 14:23:08 -0400

    Refactor openLightbox function to remove script tags

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-<script>
-    function openLightbox(src) {
-        const modal = document.getElementById('image-modal');
-        const modalImg = document.getElementById('modal-img');
-        modalImg.src = src;
-        modal.showModal();
-    }
-</script>
+function openLightbox(src) {
+    const modal = document.getElementById('image-modal');
+    const modalImg = document.getElementById('modal-img');
+    modalImg.src = src;
+    modal.showModal();
+}
