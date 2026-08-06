7c6d9ff337bb6a2ca6f0204762a910ce9ed939d5
Jose M. <joseernestomadlangbayan@gmail.com>
2026-08-06 09:45:16 -0400

    Add toggleAnswer function for question answers

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+function toggleAnswer(question) {
+  const answer = question.nextElementSibling;
+  const icon = question.querySelector(".qa-toggle");
 
+  answer.classList.toggle("active");
+  icon.textContent = answer.classList.contains("active") ? "▲" : "▼";
+}
